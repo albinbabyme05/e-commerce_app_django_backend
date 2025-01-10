cd9ab82f3cc4756e75a2a33d3bae1e4fb9a3bd57
Albin Baby <albinbabayme05@gmail.com>
2025-01-11 00:21:04 +0100

    templates are added for the app

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 
 urlpatterns = [ 
-               path('', views.index, name='index'),
-            #    path('about/', views.about, name='about'),
-            #    path('events/', views.events, name='events'),
+               path('', views.index, name='home'),
+               path('product_list/', views.productList, name='product_list'),
+               path('products_details/', views.detailed_product_view, name='product_details'),
+            
             #    path('contact/', views.contact, name='contact'),
             #    path('booking/', views.booking, name='booking'),
                ]
--- products/views.py
+++ products/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+# return product list
+def productList(request):
+    return render(request, 'products.html')
+
+# return detailed view of  product
+def detailed_product_view(request):
+    return render(request, 'product_details.html')
