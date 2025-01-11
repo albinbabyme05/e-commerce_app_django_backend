b11ef4dbcf14d8a9eaf4b114d225627d97a075de
Albin Baby <albinbabayme05@gmail.com>
2025-01-11 17:00:36 +0100

    pagination and products show as per the request implimeted

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Product
+
+# implimetting pagiator
+from django.core.paginator import Paginator
 
 #render html pages
 
@@ -8,9 +11,18 @@ def index(request):
 
 # return product list
 def productList(request):
-    product_dict = {
-        'products' : Product.objects.all()
-    }
+    #base on the user request fetching the page
+    page = 1
+    
+    if request.GET:
+        page = request.GET.get('page', 1)
+    
+    product_list  = Product.objects.all() 
+    
+    product_paginator  =  Paginator(product_list, 1)
+    product_list = product_paginator.get_page(page)
+    
+    product_dict = {'products' : product_list}
     return render(request, 'products.html', product_dict)
 
 # return detailed view of  product
