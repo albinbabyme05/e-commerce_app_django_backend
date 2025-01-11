f0d8398f6966ee82d56468d4280cb715c543c917
Albin Baby <albinbabayme05@gmail.com>
2025-01-11 12:31:40 +0100

    set created for product components

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
                path('', views.index, name='home'),
                path('product_list/', views.productList, name='product_list'),
                path('products_details/', views.detailed_product_view, name='product_details'),
-            
-            #    path('contact/', views.contact, name='contact'),
-            #    path('booking/', views.booking, name='booking'),
                ]
