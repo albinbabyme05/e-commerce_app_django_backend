bb7cb9e4af87d558807af82b3ca91cb1e974277e
Albin Baby <albinbabayme05@gmail.com>
2025-01-11 15:09:28 +0100

    oder model and it functionality defeined

--- epp/urls.py
+++ epp/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('customer/', include('customers.urls')),
+    path('orders/', include('orders.urls')),
     
 ] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_carts(request):
+    return render(request, 'cart.html')
