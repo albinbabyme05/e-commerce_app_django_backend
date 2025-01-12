7cbb4f8d7558d21fbaca39db840b6bd34f6f1559
Albin Baby <albinbabayme05@gmail.com>
2025-01-12 14:58:02 +0100

    patha added for home and logout

--- customers/urls.py
+++ customers/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [ 
             path('account/', views.show_account, name='account'),
-
-               ]
+            path('logout/', views.user_logout, name='logout'),
+            path('', views.home, name='home'),
+            ]
