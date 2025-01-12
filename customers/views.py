9c027745b3b93ddc3501fd91c1b56c410f886abf
Albin Baby <albinbabayme05@gmail.com>
2025-01-12 13:53:52 +0100

    account Post request creates new user

--- customers/views.py
+++ customers/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from . models import Customer
 
 # custmorer
 
 def show_account(request):
+    if request.POST and 'register' in request.POST:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        
+
+        
+        user_reg = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email
+        )
+        print(user_reg)
+        #create customer and model
+        customer=Customer.objects.create(
+            user=user_reg,
+            phone=phone,
+            address=address
+        )
+        return redirect('home')
+        
+        
     return render(request, 'account.html')
